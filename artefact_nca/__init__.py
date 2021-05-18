63a2f4af7f69190cc332e421a0354ad5e2494e4a
Shyam Sudhakaran <shyamsnair@protonmail.com>
2021-05-17 18:25:23 -0700

    changed email + __init__.py title

--- artefact_nca/__init__.py
+++ artefact_nca/__init__.py
@@ -1,5 +1,10 @@
-"""artefact_nca - A template for deep learning projects utilizing hydra for configuration, ray for execution, mlflow, tensorboard, and w&b for metrics + artifact collection"""
+"""
+    artefact_nca
+    -------------------------------------------------------------------------------------
+    Code for 'Growing 3D Artefacts and Functional Machines with Neural Cellular Automata'
+    https://arxiv.org/abs/2103.08737
+"""
 
 __version__ = "0.1.0"
-__author__ = "Shyam Sudhakaran <shyamsnair97@gmail.com>"
+__author__ = "Shyam Sudhakaran <shyamsnair@protonmail.com>"
 __all__ = []
